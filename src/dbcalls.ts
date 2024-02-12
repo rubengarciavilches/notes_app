72429bff1734505c3f1509c7af0af74c5bab1939
Rubén García <37160008+rubengarciavilches@users.noreply.github.com>
2024-02-12 03:37:50 +0100

    NoteIt decoupled
    NoteIt decoupled to a module, now proceeding to try routing

--- src/dbcalls.ts
+++ src/dbcalls.ts
@@ -50,212 +50,30 @@ export async function getUser(userId: string): Promise<User | undefined> {
     return tryFetch("getUser", `user/${userId}`, "GET", undefined);
 }
 
-// export async function getUser(userId: string): Promise<User | null> {
-//     const url = `http://localhost:8090/api/v1/user/${userId}`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             credentials: 'include',  // Automatically includes cookies
-//         });
-//         if (!response.ok)
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         return await response.json();
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
-
 export async function register(email: string, password: string, username: string): Promise<string | undefined> {
     return tryFetch("signUp", `user/signup`, "POST", {email, password, username}, false);
 }
-
-// export async function register(email: string, password: string, username: string): Promise<string | null> {
-//     const url = `http://localhost:8090/api/v1/user/register`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 email,
-//                 password,
-//                 username
-//             }),
-//         });
-//
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//
-//         return await response.json();
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
 
 export async function registerGuest(): Promise<Token | undefined> {
     return tryFetch("signUpGuest", "user/signup/guest", "POST", undefined, false);
 }
 
-// export async function registerGuest(): Promise<Token | null> {
-//     const url = `http://localhost:8090/api/v1/user/register/guest`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'POST',
-//         });
-//
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//
-//         return await response.json();
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
-
 export async function authUser(email: string, password: string): Promise<Token | undefined> {
     return tryFetch("auth", "auth", "POST", {email, password}, false);
 }
-
-// export async function authUser(email: string, password: string): Promise<Token | undefined> {
-//     //url, method(POST/GET/DELETE), ResponseType, body, name(auth)
-//     const url = `http://localhost:8090/api/v1/auth`;
-//     let response: Response;
-//     try {
-//         response = await fetch(url, {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 email,
-//                 password
-//             }),
-//         });
-//     } catch (error) {
-//         throw new CustomError("Error during auth, couldn't reach API, please try again later.", 503);
-//     }
-//
-//     const authResponse: BaseResponse<Token> = await response.json();
-//
-//     if (!response.ok) { //(UserNotFoundException e) = 404, (IncorrectCredentialsException e) = 401
-//         if (authResponse && authResponse.errorMessage) {
-//             throw new CustomError(authResponse.errorMessage, response.status);
-//         } else {
-//             console.error(`Unexpected HTTP error during auth! Response: ${response}`);
-//             throw new CustomError(`Unexpected HTTP error during auth! Response: ${response}`);
-//         }
-//     }
-//     return authResponse.content; //token, user_id, expires_at
-// }
 
 export async function getAllNotes(userId: string): Promise<Note[] | undefined> {
     return tryFetch("getAllNotes", `note/${userId}`, "GET", undefined);
 }
 
-// export async function getAllNotes(userId: string): Promise<Note[] | null> {
-//     const url = `http://localhost:8090/api/v1/note/${userId}`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'GET',
-//             credentials: 'include',  // Automatically includes cookies
-//         });
-//         if (!response.ok)
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         return await response.json();
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
-
 export async function addNewNote(userId: string, title: string, content: string): Promise<Note | undefined> {
     return tryFetch("addNote", `note/${userId}`, "POST", {title, content});
 }
-
-// export async function addNewNote(userId: string, title: string, content: string): Promise<Note | null> {
-//     const url = `http://localhost:8090/api/v1/note/${userId}`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'POST',
-//             credentials: 'include',  // Automatically includes cookies
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 title,
-//                 content
-//             }),
-//         });
-//
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//
-//         return await response.json(); //token, user_id, expires_at
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
 
 export async function updateNote(userId: string, noteId: string, title: string, content: string): Promise<Note | undefined> {
     return tryFetch("updateNote", `note/${userId}/${noteId}`, "PUT", {title, content});
 }
 
-// export async function updateNote(userId: string, noteId: string, title: string, content: string): Promise<Note | null> {
-//     const url = `http://localhost:8090/api/v1/note/${userId}/${noteId}`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'PUT',
-//             credentials: 'include',  // Automatically includes cookies
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//             body: JSON.stringify({
-//                 title,
-//                 content
-//             }),
-//         });
-//
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//
-//         return await response.json(); //token, user_id, expires_at
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
-
 export async function deleteNote(userId: string, noteId: string): Promise<boolean | undefined> {
     return tryFetch("deleteNote", `note/${userId}/${noteId}`, "DELETE", undefined);
 }
-
-// export async function deleteNote(userId: string, noteId: string): Promise<boolean | null> {
-//     const url = `http://localhost:8090/api/v1/note/${userId}/${noteId}`;
-//     try {
-//         const response = await fetch(url, {
-//             method: 'DELETE',
-//             credentials: 'include',
-//         });
-//
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//
-//         return response.ok;
-//     } catch (error) {
-//         console.error('Error fetching user:', error);
-//         return null;
-//     }
-// }
---
+++ src/noteit/NoteItApp.tsx
@@ -0,0 +1,131 @@
+import GridNote from "./components/GridNote";
+import FloatingNote from "./components/FloatingNote";
+import {Note} from "../types";
+import NewNote from "./components/NewNote";
+import BasicAuth from "./components/BasicAuth";
+import {addNewNote, deleteNote, getAllNotes, updateNote} from "../dbcalls";
+import {useSession} from "../SessionContext";
+import React, {useEffect, useState} from "react";
+import './NoteIt.scss'
+
+function NoteItApp(){
+    const {session, setSession} = useSession();
+    const [activeNote, setActiveNote] = useState<Note | null>(null);
+    const [notes, setNotes] = useState<Note[]>([
+        {
+            id: "1",
+            user_id: "1",
+            title: "Note Title",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat.",
+            created_at: "2024-01-28T14:23:34.768883",
+            updated_at: "2024-01-28T14:23:34.768883"
+        },
+        {
+            id: "2",
+            user_id: "1",
+            title: "Note Title",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat.",
+            created_at: "2024-01-28T14:23:34.768883",
+            updated_at: "2024-01-28T14:23:34.768883"
+        },
+        {
+            id: "3",
+            user_id: "1",
+            title: "Note Title",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat.",
+            created_at: "2024-01-28T14:23:34.768883",
+            updated_at: "2024-01-28T14:23:34.768883"
+        },
+        {
+            id: "4",
+            user_id: "1",
+            title: "Note Title",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum varius nulla. Aliquam eu volutpatpurus. Nullam luctus feugiat viverra. Curabitur a mauris scelerisque, egestas justo sed, lobortis erat.",
+            created_at: "2024-01-28T14:23:34.768883",
+            updated_at: "2024-01-28T14:23:34.768883"
+        },
+    ]);
+
+    useEffect(() => {
+        console.log("Trying to retrieve session")
+        if (!session){
+            setNotes([]);
+            return;
+        }
+        console.log(session);
+        getAllNotes(session.user.id).then((notes) => {
+            if (!notes) return;
+            setNotes(notes);
+        });
+    }, [session]);
+
+    function handleCloseFloatingNote() {
+        setActiveNote(null);
+    }
+
+    function handleDeleteNote(note: Note) {
+        if(!session) return;
+        deleteNote(session.user.id, note.id).then(() => {
+            console.log("Note deleted.");
+            let newNotes: Note[] = notes.filter((tempNote) => tempNote.id !== note.id)
+            setNotes(newNotes);
+            handleCloseFloatingNote();
+        })
+    }
+
+    function handleOpenFloatingNote(note: Note) {
+        setActiveNote(note);
+    }
+
+    function handleSaveNewNote(title: string, content: string) {
+        if (!session) return;
+        addNewNote(session.user.id, title, content).then((note) => {
+            if (!note) return;
+            console.log("Added note: ", note);
+            setNotes(notes.concat(note));
+        });
+    }
+
+    function handleUpdateNote(note: Note) {
+        if (!session) return;
+        updateNote(session.user.id, note.id, note.title, note.content).then((note) => {
+            if (!note) return;
+            const updatedNotes = notes.map(noteTemp =>
+                noteTemp.id === note.id ? {...noteTemp, title: note.title, content: note.content} : noteTemp
+            );
+            setNotes(updatedNotes);
+        });
+    }
+
+    return (
+        <div className="container">
+            <div className={"top-menu"}>
+                <div>
+                    <h1>NoteIt</h1>
+                    <h2>Easily organize all your notes</h2>
+                </div>
+                <BasicAuth></BasicAuth>
+            </div>
+            <NewNote onSave={handleSaveNewNote}></NewNote>
+            <div className={"grid-container"}>
+                {notes
+                    .slice() //Generates a copy of the array
+                    // .sort((a, b) => b.id - a.id) //TODO sort by date updated
+                    .map((note) => (
+                        <GridNote key={note.id} note={note} onClick={handleOpenFloatingNote}></GridNote>
+                    ))}
+            </div>
+            <div className={activeNote ? "grayed-out-background" : ""}></div>
+            {activeNote != null && (
+                <FloatingNote
+                    note={activeNote}
+                    onClose={handleCloseFloatingNote}
+                    onDelete={handleDeleteNote}
+                    onSave={handleUpdateNote}
+                ></FloatingNote>
+            )}
+        </div>
+    );
+}
+
+export default NoteItApp;
