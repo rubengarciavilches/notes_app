223774765f8ee20a9c296e23fb2c11af98b73dfa
Rubén García <37160008+rubengarciavilches@users.noreply.github.com>
2024-02-12 05:31:20 +0100

    Started Valentine

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
-// import './App.css';
 import './App.scss'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
 import NoteItApp from "./noteit/NoteItApp";
+import DisplayValentineApp from "./valentine/DisplayValentineApp";
+import CreateValentineApp from "./valentine/CreateValentineApp";
+
+function getSubdomain() {
+    const parts = window.location.hostname.split('.');
+    console.log(parts);
+    if (parts.length > 1) {
+        return parts[0];
+    } else {
+        return 'www'; // Default subdomain
+    }
+}
 
 function App() {
-    return (
-        <NoteItApp></NoteItApp>
-    );
+    const subdomain = getSubdomain();
+    console.log("The subdomain is " + subdomain);
+    switch (subdomain) {
+        case "valentine":
+            return <Router>
+                <Routes>
+                    <Route path={"/:valentineId"} Component={DisplayValentineApp}> </Route>
+                    <Route path={"/"} Component={CreateValentineApp}> </Route>
+                </Routes>
+            </Router>
+        case "noteit":
+            return <NoteItApp></NoteItApp>
+        default:
+            return <NoteItApp></NoteItApp>
+    }
 }
 
 export default App;
